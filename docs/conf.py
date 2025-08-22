6340d6beda5d87210e538cea674e1f82368ffe26
Paveljolak <jolak235@hotmail.com>
2025-08-22 02:28:14 +0200

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,4 @@
-
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
 from git import Repo
@@ -13,32 +8,36 @@ project = 'my-docs'
 copyright = '2025, pavel'
 author = 'pavel'
 release = '1.0.0'
+version = release  # Added for EPUB compatibility
 
 # -- General configuration ---------------------------------------------------
 extensions = ['sphinx_rtd_theme']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# add source code path
+# Add source code path
 sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']  # Commented out to avoid warning; uncomment if custom static files are needed
 
 # -------------------------------
 # Multi-version (branch) support
 # -------------------------------
-
 try:
     html_context
 except NameError:
     html_context = dict()
 
 html_context['display_lower_left'] = True
+html_context['display_github'] = True
+html_context['github_user'] = 'Paveljolak'
+html_context['github_repo'] = 'rtd-tryF'
+html_context['github_version'] = 'main/docs/'
 
 # Repo name for URLs
-REPO_NAME = os.environ.get('REPO_NAME', '')
+REPO_NAME = os.environ.get('REPO_NAME', 'rtd-tryF')
 
 # Single language build (English)
 current_language = os.environ.get('current_language', 'en')
@@ -52,21 +51,12 @@ html_context['version'] = current_version
 
 # Links to other versions (branches)
 html_context['versions'] = [
-    (branch.name, f'/{REPO_NAME}/{current_language}/{branch.name}/')
-    for branch in repo.branches
+    ('main', f'/{REPO_NAME}/{current_language}/main/'),
+    ('jazzy', f'/{REPO_NAME}/{current_language}/jazzy/')
 ]
 
 # Links to downloads (PDF/EPUB)
 html_context['downloads'] = [
-    ('pdf', f'/{REPO_NAME}/{current_language}/{current_version}/{project}-docs_{current_language}_{current_version}.pdf'),
-    ('epub', f'/{REPO_NAME}/{current_language}/{current_version}/{project}-docs_{current_language}_{current_version}.epub')
+    ('pdf', f'/{REPO_NAME}/{current_language}/{current_version}/my-docs_{current_language}_{current_version}.pdf'),
+    ('epub', f'/{REPO_NAME}/{current_language}/{current_version}/my-docs_{current_language}_{current_version}.epub')
 ]
-##########################
-# "EDIT ON GITHUB" LINKS #
-##########################
- 
-html_context['display_github'] = True
-html_context['github_user'] = 'Paveljolak'
-html_context['github_repo'] = 'rtd-tryF'
-html_context['github_version'] = 'main/docs/'
- 
